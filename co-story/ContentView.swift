86c42d76d34698ab56f6003dca935fd6b9ea37a5
Songwut Maneefun <songwut.m@gmail.com>
2020-02-25 16:26:10 +0700

    add files

---
+++ co-story/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  co-story
+//
+//  Created by Songwut on 25/2/20.
+//  Copyright © 2020 conicle. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ co-story/InternalLib/Transitions/AnimationControllers/FlipDismissAnimationController.swift
@@ -0,0 +1,74 @@
+//
+//  FlipDismissAnimationController.swift
+//  GuessThePet
+//
+//  Created by Vesza Jozsef on 08/07/15.
+//  Copyright (c) 2015 Razeware LLC. All rights reserved.
+//
+
+import UIKit
+
+class FlipDismissAnimationController: NSObject, UIViewControllerAnimatedTransitioning {
+  
+  var destinationFrame = CGRectZero
+  
+  func transitionDuration(transitionContext: UIViewControllerContextTransitioning?) -> NSTimeInterval {
+    return 0.6
+  }
+  
+  func animateTransition(transitionContext: UIViewControllerContextTransitioning) {
+    
+    guard let fromVC = transitionContext.viewControllerForKey(UITransitionContextFromViewControllerKey),
+        let containerView = transitionContext.containerView(),
+        let toVC = transitionContext.viewControllerForKey(UITransitionContextToViewControllerKey) else {
+        return
+    }
+    
+    let finalFrame = destinationFrame
+    
+    let snapshot = fromVC.view.snapshotViewAfterScreenUpdates(false)
+    
+    snapshot.layer.cornerRadius = 25
+    snapshot.layer.masksToBounds = true
+    
+    containerView.addSubview(toVC.view)
+    containerView.addSubview(snapshot)
+    fromVC.view.hidden = true
+    
+    AnimationHelper.perspectiveTransformForContainerView(containerView)
+    
+    //toVC.view.layer.transform = AnimationHelper.yRotation(-M_PI_2)
+    
+    let duration = transitionDuration(transitionContext)
+    
+    UIView.animateKeyframesWithDuration(
+      duration,
+      delay: 0,
+      options: .CalculationModeCubic,
+      animations: {
+        
+        UIView.addKeyframeWithRelativeStartTime(0.0, relativeDuration: 1, animations: {
+            snapshot.frame = finalFrame
+            snapshot.alpha = 0
+        })
+        /*
+        UIView.addKeyframeWithRelativeStartTime(0.0, relativeDuration: 1/3, animations: {
+          snapshot.frame = finalFrame
+        })
+ 
+        UIView.addKeyframeWithRelativeStartTime(1/3, relativeDuration: 1/3, animations: {
+          //snapshot.layer.transform = AnimationHelper.yRotation(M_PI_2)
+        })
+        
+        UIView.addKeyframeWithRelativeStartTime(2/3, relativeDuration: 1/3, animations: {
+          //toVC.view.layer.transform = AnimationHelper.yRotation(0.0)
+        })
+ */
+      },
+      completion: { _ in
+        fromVC.view.hidden = false
+        snapshot.removeFromSuperview()
+        transitionContext.completeTransition(!transitionContext.transitionWasCancelled())
+      })
+  }
+}
